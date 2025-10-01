8a47378748baec6fac535157647c22e68cb107dd
xKirtle <rodm.martins@proton.me>
2025-10-01 18:54:27 +0100

    Fix installation path in release script for local builds

--- release-locally.sh
+++ release-locally.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 go build -o hypr-local-workspaces ./cmd/hypr-local-workspaces
-sudo install -Dm755 "hypr-local-workspaces" "$pkgdir/usr/bin/hypr-local-workspaces"
+sudo install -Dm755 "hypr-local-workspaces" "/usr/bin/hypr-local-workspaces"
 ls -l /usr/bin | grep hypr-local-workspaces
