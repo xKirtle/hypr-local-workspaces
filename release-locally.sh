9cb9d3f383e4986bab72fb6f5010d215d175daae
xKirtle <rodm.martins@proton.me>
2025-10-17 02:34:38 +0100

    Fix release script: Add newline at end of file and ensure cleanup of local binary after installation

--- release-locally.sh
+++ release-locally.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 go build -o hypr-local-workspaces ./cmd/hypr-local-workspaces
 sudo install -Dm755 "hypr-local-workspaces" "/usr/bin/hypr-local-workspaces"
 ls -l /usr/bin | grep hypr-local-workspaces
+rm hypr-local-workspaces
